222b8d14a5c5f26bb6804679577418136e432e24
teotia-codes <priyanshu.teotia50@gmail.com>
2024-02-21 12:09:14 +0530

    done with adding alert dialog and add item button

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.shoppinglistapp"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.shoppinglistapp"
